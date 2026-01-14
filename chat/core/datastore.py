e394a80d8fcbfea1ea052f2d6e8aa1b75f4c56e5
MUstinov77 <m.ustinov2001@yandex.ru>
2026-01-14 10:14:21 +0300

    create db dependency

---
+++ chat/core/datastore.py
@@ -0,0 +1,40 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncEngine, AsyncSession
+from fastapi import Depends
+from typing import Annotated
+
+async_session_maker: async_sessionmaker[AsyncSession] | None = None
+async_engine: AsyncEngine | None = None
+
+
+async def init_db():
+    global async_engine, async_session_maker
+
+    async_engine = create_async_engine(
+        "postgresql+asyncpg://postgres:postgres@localhost:5432/postgres",
+    )
+
+    async_session_maker = async_sessionmaker(async_engine)
+
+
+async def clean_db():
+    global async_engine
+
+    if async_engine:
+        await async_engine.dispose()
+        async_engine = None
+
+
+async def get_postgres_session():
+    async with async_session_maker() as session:
+        try:
+            yield session
+            await session.commit()
+        except Exception:
+            await session.rollback()
+            raise
+
+
+async def async_session_provider(
+        session: Annotated[AsyncSession, Depends(get_postgres_session)]
+):
+    return session
