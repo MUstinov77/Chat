c408cb22ab52931c3c62ba34dac4bc90f9375429
MUstinov77 <m.ustinov2001@yandex.ru>
2026-01-14 10:01:47 +0300

    add rel to chat model

--- chat/models/chat.py
+++ chat/models/chat.py
@@ -1,15 +1,24 @@
 from datetime import datetime
 
 from chat.models.base import Base
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String, DateTime, func
 
 
 class Chat(Base):
 
+    __tablename__ = "chats" # noqa
+
     id: Mapped[int] = mapped_column(primary_key=True)
-    title: Mapped[str] = mapped_column(String(), nullable=False)
+    title: Mapped[str] = mapped_column(String(200), nullable=False)
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
         default=func.now()
+    )
+
+    messages = relationship(
+        "Message",
+        uselist=True,
+        back_populates="chat",
+        cascade="all, delete"
     )
