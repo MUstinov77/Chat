b8f0a44366655269db99b4d5fb42375f349140e3
MUstinov77 <m.ustinov2001@yandex.ru>
2026-01-14 09:43:07 +0300

    chat sql class

---
+++ chat/models/chat.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from chat.models.base import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, DateTime, func
+
+
+class Chat(Base):
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=func.now()
+    )
